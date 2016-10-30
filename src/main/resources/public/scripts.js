d054a0905a96ef49441f050f00e0b1b4f7d7d83e
Gardar Mar <gardarmar@i-1217c9a8.cs1004cloud.internal>
2016-10-30 21:50:25 +0000

    bæta við í javascript tli að reyna laga winner

--- src/main/resources/public/scripts.js
+++ src/main/resources/public/scripts.js
@@ -1,6 +1,70 @@
 board = {
     currentPlayer: "X",
     init: function() {
+        board.resetGame();
+        board.bind();
+    },
+    bind: function() {
+        // Bind click on board-cell.
+        $(".board-cell").click(function(e) {
+            if (!$(this).hasClass('checked') && !$(".board-board").hasClass("disabled")) {
+                board.handleMove($(this).attr('data-value'));
+                if (board.currentPlayer == "X") {
+                    $(this).addClass('cross checked');
+                } else {
+                    $(this).addClass('circle checked');
+                }
+                $(".board-board").addClass("disabled");
+            }
+        });
+        $("#board-reset").click(function() {
+            board.resetGame();
+        });
+    },
+    handleMove: function(cell) {
+        // Handle cell clicked
+        $.ajax({
+            type: 'post',
+            url: '/handleMove',
+            data: 'cell=' + cell
+        }).done(function(result) {
+            if (result != "") {
+                $("#board-status").html(result);
+            } else {
+                board.currentPlayer = board.currentPlayer == "X" ? "O" : "X";
+                board.updateCurrentPlayerStatus();
+                $(".board-board").removeClass("disabled");
+            }
+        }).fail(function() {
+            $("#board-status").html("Error!").addClass("alert alert-danger");
+        });
+    },
+    updateCurrentPlayerStatus: function() {
+        // Update the current player
+        $("#board-status").html("It's your turn, " + board.currentPlayer + "!");
+    },
+    resetGame: function() {
+        $.ajax({
+            type: 'post',
+            url: '/resetGame'
+        }).done(function(result) {
+            $(".board-cell").removeClass('cross circle checked');
+            $(".board-board").removeClass("disabled");
+        }).fail(function() {
+            $("#board-status").html("Error!").addClass("alert alert-danger");
+        });
+
+        board.currentPlayer = "X";
+        board.updateCurrentPlayerStatus();
+    }
+};
+
+$(document).ready(function() {
+    board.init();
+});
+board = {
+    currentPlayer: "X",
+    init: function() {
         board.bind();
         board.resetGame();
     },
@@ -37,7 +101,7 @@ board = {
                 $status = $("#board-status");
                 if (result == "X has won!") {
                     $status.html(result).addClass('xwon');
-                } else if (result == "O has won!" || result == "o has won!") {
+                } else if (result == "O has won!") {
                     $status.html(result).addClass('owon');
                 } else {
                     $status.html(result).addClass('tie');
