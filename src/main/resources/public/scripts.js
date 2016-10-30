958988e02642023ef25780e1fa22bc3acfb89929
Gardar Mar <gardarmar@i-1217c9a8.cs1004cloud.internal>
2016-10-30 19:46:26 +0000

    uppfæra .js

--- src/main/resources/public/scripts.js
+++ src/main/resources/public/scripts.js
@@ -1,22 +1,27 @@
 board = {
     currentPlayer: "X",
     init: function() {
-        board.resetGame();
         board.bind();
+        board.resetGame();
     },
     bind: function() {
         // Bind click on board-cell.
         $(".board-cell").click(function(e) {
             if (!$(this).hasClass('checked') && !$(".board-board").hasClass("disabled")) {
-                board.handleMove($(this).attr('data-value'));
+                // Don't allow another move until the previous one has been handled.
+                $(".board-board").addClass("disabled");
+
                 if (board.currentPlayer == "X") {
                     $(this).addClass('cross checked');
                 } else {
                     $(this).addClass('circle checked');
                 }
-                $(".board-board").addClass("disabled");
+
+                board.handleMove($(this).attr('data-value'));
             }
         });
+
+        // Bind the reset game button click..
         $("#board-reset").click(function() {
             board.resetGame();
         });
@@ -29,7 +34,14 @@ board = {
             data: 'cell=' + cell
         }).done(function(result) {
             if (result != "") {
-                $("#board-status").html(result);
+                $status = $("#board-status");
+                if (result == "X has won!") {
+                    $status.html(result).addClass('xwon');
+                } else if (result == "O has won!") {
+                    $status.html(result).addClass('owon');
+                } else {
+                    $status.html(result).addClass('tie');
+                }
             } else {
                 board.currentPlayer = board.currentPlayer == "X" ? "O" : "X";
                 board.updateCurrentPlayerStatus();
@@ -44,22 +56,24 @@ board = {
         $("#board-status").html("It's your turn, " + board.currentPlayer + "!");
     },
     resetGame: function() {
+        $status = $("#board-status");
+        $status.removeClass("xwon owon tie");
+        $status.removeClass("alert alert-danger");
+
         $.ajax({
             type: 'post',
             url: '/resetGame'
-        }).done(function(result) {
+        }).done(function() {
             $(".board-cell").removeClass('cross circle checked');
             $(".board-board").removeClass("disabled");
+            board.currentPlayer = "X";
+            board.updateCurrentPlayerStatus();
         }).fail(function() {
-            $("#board-status").html("Error!").addClass("alert alert-danger");
+            $status.html("Error!").addClass("alert alert-danger");
         });
-
-        board.currentPlayer = "X";
-        board.updateCurrentPlayerStatus();
     }
 };
 
 $(document).ready(function() {
     board.init();
 });
-
